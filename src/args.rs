24f97cfaaa5066417994954d5175586051d73bf2
Maciej Śniegocki <m.w.sniegocki@gmail.com>
2021-12-15 14:14:44 +0100

    refactor: sort fields of ProgramArgs

--- src/args.rs
+++ src/args.rs
@@ -3,12 +3,12 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "partikl")]
 pub struct ProgramArgs {
+    #[structopt(long)]
+    pub exit_after: Option<f64>,
+
     #[structopt(short = "n", long, default_value = "1000")]
     pub number_of_particles: usize,
 
     #[structopt(long, default_value = "1")]
     pub parallel_batch_size: usize,
-
-    #[structopt(long)]
-    pub exit_after: Option<f64>,
 }
