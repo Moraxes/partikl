585f4f9d414d5df0214545fd4426b676dde0e229
Maciej Śniegocki <m.w.sniegocki@gmail.com>
2021-12-15 14:30:10 +0100

    document arguments (also adds help messages)

--- src/args.rs
+++ src/args.rs
@@ -3,12 +3,17 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "partikl")]
 pub struct ProgramArgs {
-    #[structopt(long)]
-    pub exit_after: Option<f64>,
+  /// Exit after this many seconds.
+  ///
+  /// This is useful for profiling/benchmarking.
+  #[structopt(long)]
+  pub exit_after: Option<f64>,
 
-    #[structopt(short = "n", long, default_value = "1000")]
-    pub number_of_particles: usize,
+  /// Number of particles to be generated initially.
+  #[structopt(short = "n", long, default_value = "1000")]
+  pub number_of_particles: usize,
 
-    #[structopt(long, default_value = "1")]
-    pub parallel_batch_size: usize,
+  /// Batch size for parallel operations.
+  #[structopt(long, default_value = "1")]
+  pub parallel_batch_size: usize,
 }
